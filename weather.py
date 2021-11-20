062ba5e4296df2bae3abe51320ad6eb469db4e2c
Łukasz Ł <ukaszek1001@gmail.com>
2021-11-20 12:14:38 +0100

    poprawiono plik weather

--- weather.py
+++ weather.py
@@ -1,30 +1,26 @@
 import requests
 import sys
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
+
 
 
 class Weather:
 
-    def __init__(self, api_key, date=str(datetime.today().date())):
+    def __init__(self, api_key, date=str(datetime.today().date()+timedelta(1))):
         self.api_key = api_key
         self.date = date
         self.data = self.get_data()
 
     def get_data(self):
-        req_url = 'http://api.weatherapi.com/v1/history.json'
+        req_url = f'https://api.meteomatics.com/{self.date}T12:00:00+01:00/precip_24h:mm/50.255689,19.022147/json'
         payload = {'key': self.api_key, "q": "London", "dt": str(self.date)}
-        r = requests.get(req_url, params=payload)
-        content = r.json()
-        return content
-
-    def get_rain_info(self):
-        totalprecip_mm = float(self.data['forecast']['forecastday'][0]['day']
-                               ['totalprecip_mm'])
-        return self.get_rain_chance(totalprecip_mm)
+        r = requests.get(req_url,auth=(self.api_key.split(':')[0], self.api_key.split(':')[1]))
+        return r.json()['data'][0]['coordinates'][0]['dates']
 
 
-    def get_rain_chance(totalprecip_mm):
+    def get_rain_chance(self):
+        totalprecip_mm = float(self.data[0]['value'])
         if totalprecip_mm > 0.0:
             return 'Bedzie padac'
         elif totalprecip_mm == 0.0:
@@ -32,14 +28,34 @@ class Weather:
         else:
             return 'Nie wiem!'
 
-
-weather = Weather(api_key=sys.argv[1], date=sys.argv[2])
 with open("forecast.json", 'r') as file:
-    odczyt = json.load(file)
+    odczyt=json.load(file)
+
+if len(sys.argv) > 2:
     if str(sys.argv[2]) not in odczyt.keys():
-        odczyt[sys.argv[2]] = weather.get_rain_info()
 
-print(odczyt[sys.argv[2]])
+        try:
+            weather = Weather(api_key=sys.argv[1], date=sys.argv[2])
+            odczyt[sys.argv[2]] = weather.get_rain_chance()
+            print(weather.get_rain_chance())
+        except:
+            print('Nie wiem')
 
-with open("forecast.json", "w") as forecast2:
-    json.dump(odczyt, forecast2)
+    else:
+        print(odczyt[sys.argv[2]])
+else:
+    date = str(datetime.today().date() + timedelta(1))
+    if date not in odczyt.keys():
+        try:
+            weather = Weather(api_key=sys.argv[1], date = date)
+            odczyt[date] = weather.get_rain_chance()
+            print(weather.get_rain_chance())
+        except:
+            print("nie wiem")
+    else:
+        print(odczyt[date])
+
+with open('forecast.json', 'w') as file:
+    json.dump(odczyt, file)
+
+
